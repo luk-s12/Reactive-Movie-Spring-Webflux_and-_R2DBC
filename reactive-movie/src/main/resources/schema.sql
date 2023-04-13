a983a57430d6d08f2ea250f267c4338e41bc687a
luk-s12 <lucas.utn12@gmail.com>
2023-04-13 14:12:36 -0300

    Added tests for MovieController endpoints

--- reactive-movie/src/main/resources/schema.sql
+++ reactive-movie/src/main/resources/schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS movies (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  created_at TIMESTAMP NOT NULL
+  created_at TIMESTAMP(3) WITH TIME ZONE NOT NULL
 );
