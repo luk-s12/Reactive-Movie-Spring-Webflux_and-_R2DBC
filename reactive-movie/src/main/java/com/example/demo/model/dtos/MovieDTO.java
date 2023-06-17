4da5e5bef539a1ef87c3c35bdca5faf5bd7f3060
luk-s12 <lucas.utn12@gmail.com>
2023-06-17 03:44:12 -0300

    Not null and required request

--- reactive-movie/src/main/java/com/example/demo/model/dtos/MovieDTO.java
+++ reactive-movie/src/main/java/com/example/demo/model/dtos/MovieDTO.java
@@ -3,34 +3,36 @@ package com.example.demo.model.dtos;
 import java.io.Serializable;
 import java.util.UUID;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
-public class MovieDTO  implements Serializable{
-	
+public class MovieDTO implements Serializable {
+
 	private static final long serialVersionUID = -3485502077061969631L;
-	
+
 	private UUID id;
-	@NotNull
+
+	@NotBlank
 	private String title;
-	@NotNull
+
+	@NotBlank
 	private String description;
-	
+
 	public UUID getId() {
 		return id;
 	}
-	
+
 	public void setId(UUID id) {
 		this.id = id;
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
@@ -43,7 +45,5 @@ public class MovieDTO  implements Serializable{
 	public String toString() {
 		return "MovieDTO [id=" + id + ", title=" + title + ", description=" + description + "]";
 	}
-	
-	
-	
+
 }
